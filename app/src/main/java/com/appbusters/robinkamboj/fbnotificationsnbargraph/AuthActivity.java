ec9df6de6a1ee252792b29843cae02bbd909d046
Robin Kamboj <dev.robinkamboj@gmail.com>
2017-01-12 23:08:03 +0530

    ..

--- app/src/main/java/com/appbusters/robinkamboj/fbnotificationsnbargraph/AuthActivity.java
+++ app/src/main/java/com/appbusters/robinkamboj/fbnotificationsnbargraph/AuthActivity.java
@@ -1,13 +1,30 @@
 package com.appbusters.robinkamboj.fbnotificationsnbargraph;
 
 import android.os.Bundle;
+import android.support.annotation.NonNull;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.util.Log;
 import android.view.View;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+
+import java.util.HashMap;
 
 public class AuthActivity extends AppCompatActivity {
+
+    private FirebaseAuth mAuth;
+    private FirebaseAuth.AuthStateListener mAuthListener;
+    private static final String TAG = "..YOLO..";
+    String uid = "some-uid";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -15,6 +32,62 @@ public class AuthActivity extends AppCompatActivity {
         setContentView(R.layout.activity_auth);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
+
+        mAuth = FirebaseAuth.getInstance();
+
+        mAuthListener = new FirebaseAuth.AuthStateListener() {
+            @Override
+            public void onAuthStateChanged(@NonNull FirebaseAuth firebaseAuth) {
+                FirebaseUser user = firebaseAuth.getCurrentUser();
+                if (user != null) {
+                    // User is signed in
+                    Log.d(TAG, "onAuthStateChanged:signed_in:" + user.getUid());
+                } else {
+                    // User is signed out
+                    Log.d(TAG, "onAuthStateChanged:signed_out");
+                }
+                // ...
+            }
+        };
+        // ...
+
+        /*
+        String uid = "some-uid";
+        HashMap<String, Object> additionalClaims = new HashMap<String, Object>();
+        additionalClaims.put("premiumAccount", true);
+
+        FirebaseAuth.getInstance().createCustomToken(uid, additionalClaims)
+                .addOnSuccessListener(new OnSuccessListener<String>() {
+                    @Override
+                    public void onSuccess(String customToken) {
+                        // Send token back to client
+                    }
+                });
+
+        FirebaseAuth.getInstance().createCustomToken(uid)
+                .addOnSuccessListener(new OnSuccessListener<String>() {
+                    @Override
+                    public void onSuccess(String customToken) {
+                        // Send token back to client
+                    }
+                });
+
+        mAuth.signInWithCustomToken(mCustomToken)
+                .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        Log.d(TAG, "signInWithCustomToken:onComplete:" + task.isSuccessful());
+
+                        // If sign in fails, display a message to the user. If sign in succeeds
+                        // the auth state listener will be notified and logic to handle the
+                        // signed in user can be handled in the listener.
+                        if (!task.isSuccessful()) {
+                            Log.w(TAG, "signInWithCustomToken", task.getException());
+                            Toast.makeText(AuthActivity.this, "Authentication failed.",
+                                    Toast.LENGTH_SHORT).show();
+                        }
+                    }
+                });
 
         FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
         fab.setOnClickListener(new View.OnClickListener() {
@@ -24,6 +97,25 @@ public class AuthActivity extends AppCompatActivity {
                         .setAction("Action", null).show();
             }
         });
+
+        */
     }
+
+    @Override
+    public void onStart() {
+        super.onStart();
+        mAuth.addAuthStateListener(mAuthListener);
+        // ...
+    }
+
+    @Override
+    public void onStop() {
+        super.onStop();
+        if (mAuthListener != null) {
+            mAuth.removeAuthStateListener(mAuthListener);
+        }
+        // ...
+    }
+
 
 }
